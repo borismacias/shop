f492476d85efb583c45bc1924200cfb5472c5469
Despair <bgonzalez@ing.uchile.cl>
2014-04-24 15:43:02 -0300

    block

--- wp-content/themes/flatsome/woocommerce/content-single-product.php
+++ wp-content/themes/flatsome/woocommerce/content-single-product.php
@@ -112,7 +112,7 @@
             do_action( 'woocommerce_after_single_product_summary' );
 
             $name = get_query_var('name');
-            echo "<div class='row'><h3 class='section-title'><span>C&oacute;mo lo uso?</span></h3>";
+            echo "<div class='row'><h3 class='section-title'><span>&iquest;C&oacute;mo lo uso?</span></h3>";
             echo do_shortcode('[block id="look-'.$name.'"]');
             echo "</div>";
             $terms = get_the_terms($post->ID,'product_cat');
