c858a14a0febecda8ab8b626d399a35fe52926dd
Despair <bgonzalez@ing.uchile.cl>
2014-04-28 16:49:46 -0400

    nuevo boton

--- wp-content/themes/flatsome/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/flatsome/woocommerce/single-product/add-to-cart/variable.php
@@ -186,7 +186,7 @@ border-color: #ccc;
 ?>
 
 <button style="position:relative;top:-14px;"href="#" class="boton_ceci eModal-<?php echo $modals[$modal]?>">Ver guía de tallas</button>
-<button class="boton_ceci tooltips" id="horma" style="position:relative;top:-14px;" >Horma</button>
+<button class="boton_ceci tooltips" title ="Hola, soy el tooltip de la horma :D asdasdasdasdasdasdasdasdasdasdasd" id="horma" style="position:relative;top:-14px;" >Horma</button>
 
 
 
