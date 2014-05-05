1737a346d1b1e63debd8969c4ca225dbbcc5712b
Despair <bgonzalez@ing.uchile.cl>
2014-05-05 16:14:38 -0400

    email header

--- wp-content/themes/flatsome/woocommerce/emails/email-header.php
+++ wp-content/themes/flatsome/woocommerce/emails/email-header.php
@@ -105,11 +105,13 @@ $header_content_h1 = "
                             	<td align="center" valign="top">
                                     <!-- Header -->
                                 	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header" style="<?php echo $template_header; ?>" bgcolor="<?php echo $base; ?>">
-                                        <tr style="background-color:black;">
+                                        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                                            <tr style="background-color:black;">
                                             <td>
                                                 <span>Ll&aacute;manos a: 09.8255.5675</span>
                                             </td>
                                         </tr>
+                                        </table>
                                        <!--  <a href="http://107.170.252.164/shop/lista-de-favoritos/" style="color:white !important;text-decoration: none !important;">Mi Lista de Favoritos</a>
                                                 <a href="http://clubtacones.cl" style="color:white !important;text-decoration: none !important;">Revista</a>
                                                 <a href="http://107.170.252.164/shop/pages/acerca-de-nosotros" style="color:white !important;text-decoration: none !important;">Acerca de Nosotros</a>
