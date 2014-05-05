5adcc303500d0c3f9297ecb51377c3287212059c
Despair <bgonzalez@ing.uchile.cl>
2014-05-05 15:02:43 -0400

    email footer

--- wp-content/themes/flatsome/woocommerce/emails/customer-new-account.php
+++ wp-content/themes/flatsome/woocommerce/emails/customer-new-account.php
@@ -27,5 +27,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 
 <!-- <p><?php printf( __( 'You can access your account area to view your orders and change your password here: %s.', 'woocommerce' ), get_permalink( wc_get_page_id( 'myaccount' ) ) ); ?></p>
  -->
- <a href="<?php get_permalink( wc_get_page_id( 'myaccount' ));?>">Ir de shopping!</a>
+ <form action ="<?php echo get_permalink( wc_get_page_id( 'myaccount' ));?>">
+ 	<input type="submit" value="Ir de shopping!">
+ </form>
+
 <?php do_action( 'woocommerce_email_footer' ); ?>
