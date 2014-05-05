d9cf5d54230b7665b2307f12e30b1856c00c63fd
Despair <bgonzalez@ing.uchile.cl>
2014-05-05 16:56:16 -0400

    email header

--- wp-content/themes/flatsome/woocommerce/emails/email-header.php
+++ wp-content/themes/flatsome/woocommerce/emails/email-header.php
@@ -107,9 +107,9 @@ $header_content_h1 = "
                                     <table border="0" cellpadding="0" cellspacing="0" width="100%">
                                          <tr align="right" style="background-color:black;">
                                             <td>
-                                                <a href="http://clubtacones.cl" style="color:white !important;text-decoration: none !important;">Revista</a>
-                                                <a href="http://107.170.252.164/shop/pages/acerca-de-nosotros" style="color:white !important;text-decoration: none !important;">Acerca de Nosotros</a>
-                                                <a href="http://107.170.252.164/shop/pages/contacto" style="color:white !important;text-decoration: none !important;">Cont&aacute;ctanos</a>
+                                                <a href="http://clubtacones.cl" style="color:white !important;text-decoration: none !important;padding-right:5px;font-size:14px;">Revista</a>
+                                                <a href="http://107.170.252.164/shop/pages/acerca-de-nosotros" style="color:white !important;text-decoration: none !important;padding-right:5px;font-size:14px;">Acerca de Nosotros</a>
+                                                <a href="http://107.170.252.164/shop/pages/contacto" style="color:white !important;text-decoration: none !important;padding-right:5px;font-size:14px;">Cont&aacute;ctanos</a>
                                             </td>
                                             
                                         </tr>
