3897c26ffd2afab110bf72a9083ed4f80a7b3936
Despair <bgonzalez@ing.uchile.cl>
2014-05-09 15:17:23 -0400

    agregando nombre a thumbnails

--- wp-content/themes/flatsome/woocommerce/content-product.php
+++ wp-content/themes/flatsome/woocommerce/content-product.php
@@ -98,11 +98,10 @@ $stock_status = get_post_meta($post_id, '_stock_status',true) == 'outofstock';
       <div class="info text-center">
       	<?php $product_cats = strip_tags($product->get_categories('|', '', '')); 
       		$terms = get_the_terms($post->ID,'product_cat');
-            $cat = end($terms);
-            print_r($terms);
-            $slug = $cat->Name;
+            $id_marca = max(array_keys($terms));
+            $nombre = $terms[$id_marca]->name;
       	?>
-          <!-- <h5 class="category"><?php list($firstpart) = explode('|', $product_cats); echo $firstpart; ?></h5> -->
+          <h5 class="category"><?php echo $nombre; ?></h5>
           	
 
           <div class="tx-div small"></div>
